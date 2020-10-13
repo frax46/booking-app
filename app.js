65cb0e275746386e94e2ca965aab2c246fb300de
unknown <annobilfrance@gmail.com>
2020-10-13 17:03:08 +0100

    modifing the mutation and query to get some data

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events= []
+
 app.use(bodyParser.json());
 
 app.use('/graphql',graphqlHTTP({
     schema: buildSchema(`
+    type Event {
+        _id:ID!
+        title:String!
+        description:String!
+        price:Float!
+        date:String!
+    }
+
+    input EventInput{
+        title:String!
+        descritption:String!
+        price:Float!
+        date:String!
+    }
+
     type RootQuery{
-        events : [String!]!
+        events : [Event!]!
     }
 
     type RootMutation{
-        createEvent(name:String): String
+        createEvent(eventInput: EventInput): Event
     }
     schema{
         query:RootQuery
@@ -23,11 +40,18 @@ app.use('/graphql',graphqlHTTP({
     }`),
     rootValue:{
         events:()=>{
-            return ['Romantic cooking','Sailing','All-Night Coding']
+            return events
         },
         createEvent:(args)=>{
-            const eventName = args.name;
-            return eventName
+            const event = {
+                _id: Math.random().toString(),
+                title:args.eventInput.title,
+                description:args.eventInput.description,
+                price:+args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            events.push(event)
+            return event
         }
     },
     graphiql:true
